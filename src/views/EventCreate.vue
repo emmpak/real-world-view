83239cfa6952fac71b7c204db2b6a3561cd10803
emmpak <eiliev05@yahoo.com>
2020-04-15 13:22:02 +0100

    Add state to the store and computed properties to fetch the state

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event {{ userName }}</h1>
+    <p>This event is created by {{ userName }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    },
+    userId() {
+      return this.$store.state.user.id
+    }
+  }
+}
+</script>
