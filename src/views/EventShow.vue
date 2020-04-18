5622f8b0a2a96086fcd8d32a872ec160e4752dac
emmpak <eiliev05@yahoo.com>
2020-04-18 08:55:33 +0100

    Remove vuex dependency from EventShow

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -15,28 +15,26 @@
     <h2>
       Attendees
       <span class="badge -fill-gradient">
-        {{ event.attendees ? event.attendees.length : 0 }}
+        {{
+        event.attendees ? event.attendees.length : 0
+        }}
       </span>
     </h2>
     <ul class="list-group">
-      <li
-        v-for="(attendee, index) in event.attendees"
-        :key="index"
-        class="list-item"
-      >
+      <li v-for="(attendee, index) in event.attendees" :key="index" class="list-item">
         <b>{{ attendee.name }}</b>
       </li>
     </ul>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
-
 export default {
-  props: ['id'],
-  computed: mapState({
-    event: state => state.event.event
-  })
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
 }
 </script>
 
